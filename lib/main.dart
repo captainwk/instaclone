f0f7c7127cf95ffb0b53c9368d01939bc4609a60
captaink <wcaptaink@gmail.com>
2022-06-02 20:16:00 +0900

    start instaclone

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// git test
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
